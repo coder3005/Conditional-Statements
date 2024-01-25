849e6bc1117c21748839af3980578e0c7a9a66dc
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-25 23:58:44 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,13 @@
+package conditionalstatement;
+
+public class oddeven {
+    public static void main(String[] args) {
+        int num=10;
+        if(num%2==0) {
+            System.out.println("number is even");
+        }
+        else {
+            System.out.println("number is odd");
+        }
+    }
+}
