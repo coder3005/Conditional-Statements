a462e44350f2faff10cb2cd6d12f4eae868351b2
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-25 23:58:30 +0530

    Create largesttwo.java

---
+++ largesttwo.java
@@ -0,0 +1,16 @@
+package conditionalstatement;
+
+public class largesttwo {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        if(a>b) {
+            System.out.println("a is largest");
+        }
+        if(a==b) {
+            System.out.println("a is equal to b");
+        }
+        else {
+            System.out.println("b is largest");
+        }
+    }
+}
